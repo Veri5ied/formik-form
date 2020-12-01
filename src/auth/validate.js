897e9e83228f3ebd55446fb3e5434cc3f159157d
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-01 09:53:08 +0100

    form and validation

---
+++ src/auth/validate.js
@@ -0,0 +1,37 @@
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.firstName) {
+    errors.firstName = "First name is required";
+  } else if (values.firstName.length <= 1) {
+    errors.firstName = "Invalid First name";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Last name is required";
+  } else if (values.lastName.length <= 1) {
+    errors.lastName = "Invalid Last name";
+  }
+
+  if (!values.email) {
+    errors.email = "Email address is required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length <= 10) {
+    errors.password = "Password length is weak";
+  }
+
+  if (!values.Vpassword) {
+    errors.Vpassword = "Invalid password verification";
+  } else if (values.Vpassword !== values.password) {
+    errors.Vpassword = "Passwords don't match";
+  }
+
+  return errors;
+};
+
+export default validate;
---
+++ src/component/Form.js
@@ -0,0 +1,106 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import validate from "../auth/validate";
+import { useFormik } from "formik";
+
+function Form() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      Vpassword: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <div className="bg-grey-lighter min-h-screen flex flex-col">
+        <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+          <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+            <h1 className="mb-8 text-3xl text-center">Formik Sign up</h1>
+            <input
+              type="text"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="firstName"
+              placeholder="First Name"
+              {...formik.getFieldProps("firstName")}
+            />
+
+            <input
+              type="text"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="lastName"
+              placeholder="Last Name"
+              {...formik.getFieldProps("lastName")}
+            />
+
+            <input
+              type="text"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="email"
+              placeholder="Email"
+              {...formik.getFieldProps("email")}
+            />
+
+            <input
+              type="password"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="password"
+              placeholder="Password"
+              {...formik.getFieldProps("password")}
+            />
+            <input
+              type="password"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="Vpassword"
+              placeholder="Confirm Password"
+              {...formik.getFieldProps("Vpassword")}
+            />
+
+            <button
+              type="submit"
+              className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+            >
+              Create Account
+            </button>
+
+            <div className="text-center text-sm text-grey-dark mt-4">
+              By signing up, you agree to the
+              <a
+                className="no-underline border-b border-grey-dark text-grey-dark"
+                href="#"
+              >
+                Terms of Service
+              </a>{" "}
+              and
+              <a
+                className="no-underline border-b border-grey-dark text-grey-dark"
+                href="#"
+              >
+                Privacy Policy
+              </a>
+            </div>
+          </div>
+
+          <div className="text-grey-dark mt-6">
+            Already have an account?
+            <a
+              className="no-underline border-b border-blue text-blue"
+              href="../login/"
+            >
+              Log in
+            </a>
+            .
+          </div>
+        </div>
+      </div>
+    </Form>
+  );
+}
+
+export default Form;
